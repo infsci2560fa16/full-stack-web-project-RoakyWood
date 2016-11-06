be81686956d1f54ee4c44ae2d80fccbeddafc8f8
eil <levoyageurei@hotmail.com>
2016-11-06 11:44:30 -0500

    Add multiple files

--- src/main/resources/public/js/raok.js
+++ src/main/resources/public/js/raok.js
@@ -1,3 +1,15 @@
-    function jim() {
+function agree() {
         alert("Select OK to Acknowledge that you have read the terms and agree.");
+}
+
+function blocus(){
+    if(this.value !==''){
+        document.getElementById("jim").value=''; 
     }
+}
+
+function blocus2(){
+    if(this.value !==''){
+        document.getElementById("jee").value='';
+    }
+}
