a24b632d897300b24a2791bb1df8514932df8692
eil <levoyageurei@hotmail.com>
2016-11-08 23:59:10 -0500

    Add Calculations

--- src/main/resources/public/js/raok.js
+++ src/main/resources/public/js/raok.js
@@ -55,3 +55,38 @@ var radios = document.getElementsByName('gender');
     }
 }
 
+function calculate() {
+	var inches, feet, meters;
+	var pounds, kilograms;
+	var waist, years;
+	var factor1, factor2, leanBodyMass, bodyFatWeight;
+	var BMI, BMR, bodyFatPercent;
+
+	feet   = document.getElementById("feet").value;
+	inches = document.getElementById("inches").value;
+	pounds = document.getElementById("pounds").value;
+	waist  = document.getElementById("waist").value;
+	years  = document.getElementById("years").value;
+
+	feet   = parseFloat(feet);
+	inches = parseFloat(inches);
+	pounds = parseFloat(pounds);
+	waist  = parseFloat(waist);
+	years  = parseFloat(years);
+
+	inches    = inches + 12 * feet;
+	meters    = 0.0254 * inches;
+	kilograms = 0.453592 * pounds;
+	BMI       = kilograms / (meters * meters);
+	BMR       = 66.5 + 13.75 * kilograms + 500.3 * meters - 6.755 * years;
+
+	factor1   = 1.082 * pounds + 94.42;
+	factor2   = 4.15  * waist;
+	leanBodyMass   = factor1 - factor2;
+	bodyFatWeight  = pounds  - leanBodyMass;
+	bodyFatPercent = (100.0   * bodyFatWeight) / pounds;
+
+	document.getElementById("bmi").value = BMI;
+	document.getElementById("bmr").value = BMR;
+	document.getElementById("bodyFat").value = bodyFatPercent
+} // calculate
