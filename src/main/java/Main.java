b40dfd1dcb5cfff819370c6f42abf7a228853546
eil <levoyageurei@hotmail.com>
2016-11-19 15:50:33 -0500

    Read from Subject Table

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,29 +33,39 @@ public class Main {
     get("/db", (req, res) -> {
       Connection connection = null;
       Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
+        try {
+          connection = DatabaseUrl.extract().getConnection();
 
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+          Statement stmt = connection.createStatement();
+          stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+          stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+          ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+          ArrayList<String> output = new ArrayList<String>();
+          while (rs.next()) {
+            output.add( "Read from DB: " + rs.getTimestamp("tick"));
+          }
 
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+          attributes.put("results", output);
+          return new ModelAndView(attributes, "db.ftl");
+
+          Statement stmts = connection.createStatement();
+          stmts.executeUpdate("INSERT INTO subject VALUES (DEFAULT,'Pittsburgh','PA','Male','M','75.5','1961-12-20','284','33.5','now()')");
+          ResultSet rss = stmts.executeQuery("SELECT dob FROM subject");
+          ArrayList<String> outputs = new ArrayList<String>();
+          while (rss.next()) {
+            outputs.add( "Read from DB: " + rss.getString("City"));
+          }
+
+          attributes.put("results", outputs);
+          return new ModelAndView(attributes, "db2.ftl");
+        } 
+        catch (Exception e) {
+          attributes.put("message", "There was an error: " + e);
+          return new ModelAndView(attributes, "error.ftl");
+        }  
+        finally {
+          if (connection != null) try{connection.close();} catch(SQLException e){}
         }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
     }, new FreeMarkerEngine());
-
   }
-
 }
