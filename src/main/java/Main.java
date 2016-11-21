fac15f752d1700586593147bfe8dbd1e1f155b4b
eil <levoyageurei@hotmail.com>
2016-11-21 11:24:16 -0500

    index-content

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,11 +24,13 @@ public class Main {
     get("/hello", (req, res) -> "Does this even work?");
     get("/test", (req, res) -> "This is a test of the emergency broadcast system.");
 
-    get("/", (request, response) -> {
+    get("/index-content", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
             return new ModelAndView(attributes, "index.ftl");
     }, new FreeMarkerEngine());
+
+
 
     get("/db", (req, res) -> {
       Connection connection = null;
