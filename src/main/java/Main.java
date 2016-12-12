727da201d69e5c3d0c8d3b77267ee7f37d913866
eil <levoyageurei@hotmail.com>
2016-12-12 08:15:22 -0500

    General Cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -58,13 +58,8 @@ public class Main {
       Map<String, Object> attributes = new HashMap<>();
         try {
           connection = DatabaseUrl.extract().getConnection();
-
           Statement stmts = connection.createStatement();
-
           stmts.executeUpdate("INSERT INTO subject VALUES (DEFAULT,'" + request.queryParams("City") + "','" + request.queryParams("State") + "','" + request.queryParams("Gender") + "','" + request.queryParams("Marital") + "','" + request.queryParams("Height") + "','" + request.queryParams("DOB") + "','" + request.queryParams("Starting Weight") + "','" + request.queryParams("Starting Waist") + "','" + request.queryParams("Current Date") + "')");
-          stmts.executeUpdate("COMMIT");
-          stmts.executeUpdate("SELECT pg_sleep(3)");
-
           return new ModelAndView(attributes, "db.ftl");
         } 
         catch (Exception e) {
@@ -82,10 +77,7 @@ public class Main {
         try {
           connection = DatabaseUrl.extract().getConnection();
           Statement stmts = connection.createStatement();
-          //ResultSet rss = stmts.executeQuery("SELECT * FROM subject WHERE subject_id = (SELECT MAX(subject_id) FROM subject)");
-          
           ResultSet rss = stmts.executeQuery("SELECT * FROM subject ORDER BY subject_id DESC LIMIT 1");
-          
           ArrayList<String> outputs = new ArrayList<String>();
           while (rss.next()) {
             outputs.add( "Subject: " + rss.getInt("subject_id"));
@@ -118,20 +110,7 @@ public class Main {
           connection = DatabaseUrl.extract().getConnection();
           Statement stmts = connection.createStatement();
           stmts.executeUpdate("INSERT INTO progress VALUES (DEFAULT,'" + request.queryParams("Today") + "','" + request.queryParams("Subject ID") + "','" + request.queryParams("Current Weight") + "','" + request.queryParams("Current Waist") + "','" + request.queryParams("Current Height") + "','" + request.queryParams("Age") + "')");
-          stmts.executeUpdate("COMMIT");
-          ResultSet rss = stmts.executeQuery("SELECT * FROM progress WHERE entry_id = (SELECT MAX(entry_id) FROM progress)");
-          ArrayList<String> outputs = new ArrayList<String>();
-          
-          while (rss.next()) {
-              outputs.add( "Entry ID: " + rss.getInt("entry_id"));
-              outputs.add( "Subject ID: " + rss.getInt("subject_id"));
-              outputs.add( "Date Entered: " + rss.getDate("today"));
-              outputs.add( "Current Weight: " + rss.getDouble("current_weight"));
-              outputs.add( "Current Waist: " + rss.getDouble("current_waist"));
-              outputs.add( "Height: " + rss.getDouble("height"));
-              outputs.add( "Age: " + rss.getInt("age"));
-           }
-          attributes.put("results", outputs);
+          ResultSet rss = stmts.executeQuery("SELECT * FROM progress ORDER BY entry_id DESC LIMIT 1");
           return new ModelAndView(attributes, "db2.ftl");
         } 
         catch (Exception e) {
@@ -149,7 +128,7 @@ public class Main {
         try {
           connection = DatabaseUrl.extract().getConnection();
           Statement stmts = connection.createStatement();
-          ResultSet rss = stmts.executeQuery("SELECT * FROM progress WHERE entry_id = (SELECT MAX(entry_id) FROM progress)");
+          ResultSet rss = stmts.executeQuery("SELECT * FROM progress ORDER BY entry_id DESC LIMIT 1");
           ArrayList<String> outputs = new ArrayList<String>();
           while (rss.next()) {
               outputs.add( "Entry ID: " + rss.getInt("entry_id"));
